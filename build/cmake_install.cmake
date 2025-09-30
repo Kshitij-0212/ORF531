47c75a82661de44bcffb9fd8af4370bcc175420d
Kshitij-0212 <64787002+Kshitij-0212@users.noreply.github.com>
2025-09-29 21:57:53 -0400

    HW3 Finish

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kshitij/ORF531/orflib-0.2.0
+# Install script for directory: /home/kshitij/ORF531/orflib-0.3.0/pyorflib
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,16 +40,6 @@ endif()
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/aarch64-linux-gnu-objdump")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/kshitij/ORF531/build/orflib/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/kshitij/ORF531/build/pyorflib/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
